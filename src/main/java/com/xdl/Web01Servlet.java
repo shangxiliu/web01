9dd633328510faaf38d505584b76846d866a98b2
xingdiliang <weiqi.liu@aliyun.com>
2022-02-28 17:17:31 +0800

    test....

---
+++ src/main/java/com/xdl/Web01Servlet.java
@@ -0,0 +1,19 @@
+package com.xdl;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "Web01Servlet", value = "/Web01Servlet")
+public class Web01Servlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
